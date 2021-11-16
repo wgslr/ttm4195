eaed1aed336a74e91e54202cdb4dd18a304539bc
AngelSECCLO <angel.lomeliramos@aalto.fi>
2021-11-16 19:06:25 +0100

    Added more test data to deploy.js

--- scripts/deploy.js
+++ scripts/deploy.js
@@ -5,13 +5,34 @@
         
         const contractName = 'TicketBookingSystem' // Change this for other contract
         const seats = [
-            {
-                rowNumber: 3,
-                seatNumber: 4,
-                timestamp: 500,
-                seatViewURL: "wikipedia.org",
-                price: 30,
-            }]
+    {
+      rowNumber: 3,
+      seatNumber: 4,
+      timestamp: new Date("2100-01-01").getTime(),
+      seatViewURL: "wikipedia.org",
+      price: 30,
+    },
+    {
+      rowNumber: 3,
+      seatNumber: 5,
+      timestamp: new Date("2100-01-01").getTime(),
+      seatViewURL: "wikipedia.org",
+      price: 30,
+    },
+    {
+      rowNumber: 4,
+      seatNumber: 1,
+      timestamp: new Date("2100-01-01").getTime(),
+      seatViewURL: "wikipedia.org",
+      price: 20,
+    },
+    {
+      rowNumber: 4,
+      seatNumber: 2,
+      timestamp: new Date("2100-01-01").getTime(),
+      seatViewURL: "wikipedia.org",
+      price: 20,
+    }]
         const constructorArgs = ["Alien contra Predator", 2 * 3600 * 1000, seats]    // Put constructor args (if any) here for your contract
     
         // Note that the script needs the ABI which is generated from the compilation artifact.
