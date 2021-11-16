875fb7332f4a76746dca8d44f87dd4567cdecf95
Wojciech Geisler <wojciech.geisler@gmail.com>
2021-11-16 01:18:04 +0100

    Test double minting

--- test/booking-test.js
+++ test/booking-test.js
@@ -37,4 +37,26 @@ describe("TicketBookingSystem", function () {
       ticketBookingSystem.buy(0, { value: seats[0].price - 1 })
     ).to.be.revertedWith("The price of the ticket is not correct.");
   });
+
+  it("Prevents double booking", async function () {
+    const [seller, buyer1, buyer2] = await ethers.getSigners();
+    const TicketBookingSystemFactory = await ethers.getContractFactory(
+      "TicketBookingSystem",
+      seller
+    );
+    const ticketBookingSystem = await TicketBookingSystemFactory.deploy(
+      "Lion King",
+      seats
+    );
+
+    await ticketBookingSystem.connect(buyer1).buy(0, { value: seats[0].price });
+    await expect(
+      ticketBookingSystem.connect(buyer1).buy(0, { value: seats[0].price }),
+      "A seat cannot be reserved twice by the same buyer"
+    ).to.be.revertedWith("ERC721: token already minted");
+    await expect(
+      ticketBookingSystem.connect(buyer2).buy(0, { value: seats[0].price }),
+      "A seat cannot be reserved twice by different buyers"
+    ).to.be.revertedWith("ERC721: token already minted");
+  });
 });
