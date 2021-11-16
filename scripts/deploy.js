34658e63fca7f67761d88edd086aeaa3e2845f2d
AngelSECCLO <angel.lomeliramos@aalto.fi>
2021-11-16 18:25:03 +0100

    Fixed deploy.js for Remix

--- scripts/deploy.js
+++ scripts/deploy.js
@@ -4,15 +4,15 @@
         console.log('Running deployWithWeb3 script...')
         
         const contractName = 'TicketBookingSystem' // Change this for other contract
-        const constructorArgs = ["Alien contra Predator", [
+        const seats = [
             {
                 rowNumber: 3,
                 seatNumber: 4,
-                showTimestamp: 500,
+                timestamp: 500,
                 seatViewURL: "wikipedia.org",
-                price: 30
-            }
-            ]]    // Put constructor args (if any) here for your contract
+                price: 30,
+            }]
+        const constructorArgs = ["Alien contra Predator", 2 * 3600 * 1000, seats]    // Put constructor args (if any) here for your contract
     
         // Note that the script needs the ABI which is generated from the compilation artifact.
         // Make sure contract is compiled and artifacts are generated
@@ -24,13 +24,13 @@
         let contract = new web3.eth.Contract(metadata.abi)
     
         contract = contract.deploy({
-            data: metadata.data.bytecode.object,
+            data: '0x'+metadata.data.bytecode.object,
             arguments: constructorArgs
         })
     
         const newContractInstance = await contract.send({
             from: accounts[0],
-            gas: 1500000,
+            gas: 15000000,
             gasPrice: '30000000000'
         })
         console.log('Contract deployed at address: ', newContractInstance.options.address)
